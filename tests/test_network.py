8b20a651741cadf23c977eeda80f0378814d8ad3
Lingbo Jiang <Lingbo.Jiang@csiro.au>
2025-01-31 10:11:24 +1100

    AUS-4328 Added more tests.

--- tests/test_network.py
+++ tests/test_network.py
@@ -27,8 +27,40 @@ def test_requestWMS():
     imgLen = len(res.content)
     assert (imgLen >= 10000)
 
+
+    params = {
+            "service": "WMS",
+            "version": "1.1.1",
+            "request": "GetMap",
+            "layers": "gsmlp:BoreholeView",
+            "format": "image/png",
+            "style": "",
+            "BGCOLOR": "0xFFFFFF",
+            "TRANSPARENT": "TRUE",
+            "SRS": "EPSG:4326",
+            "BBOX": "105.53333332790065,-35.033522303030146,129.01666666127286,-10.415345166691509",
+            "WIDTH": "400",
+            "HEIGHT": "400"
+            }
+
+    res = requestWMS('https://geossdi.dmp.wa.gov.au/services/ows', params, 'POST')
+    imgLen = len(res.content)
+    assert (imgLen >= 10000)
+
 def test_requestWFS():
     res = requestWFS('https://geossdi.dmp.wa.gov.au/services/ows')
     features = res.json()['features']
     assert (len(features) == 10)
 
+    params = {
+            "service": "WFS",
+            "version": "1.1.0",
+            "request": "GetFeature",
+            "typename": "gsmlp:BoreholeView",
+            "outputFormat": "json",
+            "FILTER": "<ogc:Filter><ogc:PropertyIsEqualTo matchCase=\"false\"><ogc:PropertyName>gsmlp:nvclCollection</ogc:PropertyName><ogc:Literal>true</ogc:Literal></ogc:PropertyIsEqualTo></ogc:Filter>",
+            "maxFeatures": str(2)
+            }
+    res = requestWFS('https://geossdi.dmp.wa.gov.au/services/ows', params, 'POST')
+    features = res.json()['features']
+    assert (len(features) == 2)
